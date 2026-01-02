9d6794c555e4ec2f290ea1c1f43f01678fec9d94
Anthony Elorm Zottor <aezottor@gmail.com>
2026-01-03 02:04:11 +0300

    updated entry script

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -56,7 +56,7 @@ resource "aws_instance" "myapp-server" {
   associate_public_ip_address = true
   key_name = aws_key_pair.ssh-key.key_name
 
-  user_data = file("${path.module}/entry-script.sh") #best practice to use user data scripts for initial setup
+  user_data = file("entry-script.sh") #best practice to use user data scripts for initial setup
 
   tags = {
     Name = "${var.env_prefix}-instance"
