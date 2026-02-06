715540d852bb2b801dbf40ddeda0682d2593f3ad
Anthony Elorm Zottor <aezottor@gmail.com>
2026-02-07 01:36:37 +0300

    added github actions

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,6 @@ output "aws_ami_id" {
 output "aws_instance_public_ip" {
   value = aws_instance.myapp-server.public_ip
 }
+output "server_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
